f9927cb1316d1c23eac79945a7bea7d673a99379
Mohamed <83606701+mgharib2000@users.noreply.github.com>
2021-10-21 19:11:14 +0100

    Add files via upload

---
+++ 39 - DataPoints.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = []
+print("Enter 10 x coordinates: ")
+
+for a in range(1, 11):
+    xnumber = int(input("Enter x coordinate: "))
+    x.append(xnumber)
+
+print(" ")
+
+y = []
+print("Enter 10 y coordinates: ")
+
+for b in range(1, 11):
+    ynumber = int(input("Enter y coordinate: "))
+    y.append(ynumber)
+
+print(" ")
+
+title = input("Enter title of graph: ")
+xaxis = input("Enter x-axis label: ")
+yaxis = input("Enter y-axis label: ")
+
+plt.scatter(x, y)
+plt.title(title)
+plt.xlabel(xaxis)
+plt.ylabel(yaxis)
+plt.show()
